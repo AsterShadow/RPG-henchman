0b8dce848318a985c2b53acdb75d6a051c1964a0
AsterShadow <xzanvas@gmail.com>
2024-08-18 13:10:27 -0400

    fixed action field in edit mode so they are correctly modified

--- src/components/StatBlock/CustomBlocks/Action.tsx
+++ src/components/StatBlock/CustomBlocks/Action.tsx
@@ -1,91 +1,113 @@
-import React, { useEffect, useRef, useState } from 'react';
-import TaperedRule from './TaperedRule';
-import PropertyLineString from './PropertyLineString';
+import React, { useEffect, useRef, useState } from "react";
+import TaperedRule from "./TaperedRule";
+import PropertyLineString from "./PropertyLineString";
 
 interface ActionsProps {
-    actions: Action[] | undefined | null;
-    desc?: string;
-    title?: string;
-    editMode: boolean;
-    onChange: (index: number, field: string, value: string) => void;
+  actions: Action[] | undefined | null;
+  desc?: string;
+  title?: string;
+  editMode: boolean;
+  onChange: (index: number, field: string, value: string) => void;
+  handleDesc?: (value: string) => void;
 }
 
-const Action: React.FC<ActionsProps> = ({ actions, desc, title, editMode, onChange }) => {
+const Action: React.FC<ActionsProps> = ({
+  actions,
+  desc,
+  title,
+  editMode,
+  onChange,
+  handleDesc,
+}) => {
+  if (!actions || actions.length === 0) return null;
 
-    if (!actions || actions.length === 0) return null;
+  const renderActionName = (index: number, field: string, value: string) => {
+    const [tempValue, setTempValue] = useState<string>(value || "");
 
-    const renderActionName = (index: number, field: string, value: string) => {
-        const [tempValue, setTempValue] = useState<string>(value || '');
-        const [isFocused, setIsFocused] = useState<boolean>(false);
-
-        const handleFocus = () => setIsFocused(true);
-        const handleBlur = () => {
-            setIsFocused(false);
-            onChange(index, 'desc', tempValue);
-        };
-        if (editMode) {
-            return (
-                <input
-                    type="text"
-                    value={value}
-                    onChange={(e) => onChange(index, field, e.target.value)}
-                    className="p-2 rounded border border-gray-300 w-full mb-2"
-                />
-            );
-        } else {
-            return (
-                <span className={field === 'name' ? 'font-bold italic' : ''}>
-                    {' '}{value}
-                </span>
-            );
-        }
+    const handleBlur = () => {
+      onChange(index, "desc", tempValue);
     };
-    const renderActionDesc = (index: number, field: string, value: string) => {
-        const [tempValue, setTempValue] = useState<string>(value || '');
-        const [isFocused, setIsFocused] = useState<boolean>(false);
+    if (editMode) {
+      return (
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => onChange(index, field, e.target.value)}
+          className="p-2 rounded border border-gray-300 w-full mb-2"
+          onBlur={handleBlur}
+        />
+      );
+    } else {
+      return (
+        <span className={field === "name" ? "font-bold italic" : ""}>
+          {" "}
+          {value}
+        </span>
+      );
+    }
+  };
+  const renderActionDesc = (index: number, field: string, value: string) => {
+    const [tempValue, setTempValue] = useState<string>(value || "");
+    const [isFocused, setIsFocused] = useState<boolean>(false);
 
-        const handleFocus = () => setIsFocused(true);
-        const handleBlur = () => {
-            setIsFocused(false);
-            onChange(index, 'desc', tempValue);
-        };
-        if (editMode) {
-            return (
-                <textarea
-                    value={value}
-                    onChange={(e) => onChange(index, field, e.target.value)}
-                    className="p-2 rounded border border-gray-300 w-full mb-2"
-                    onFocus={handleFocus}
-                    onBlur={handleBlur}
-                />
-            );
-        } else {
-            return (
-                <span className={field === 'name' ? 'font-bold italic' : ''}>
-                    {' '}{value}
-                </span>
-            );
-        }
+    const handleFocus = () => setIsFocused(true);
+    const handleBlur = () => {
+      setIsFocused(false);
+      onChange(index, "desc", tempValue);
     };
+    if (editMode) {
+      return (
+        <>
+          <textarea
+            value={value}
+            onChange={(e) => onChange(index, field, e.target.value)}
+            className="p-2 rounded border border-gray-300 w-full mb-2"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
+          {isFocused && (
+            <small className="text-gray-500">
+              * Supports Markdown. Use **bold**, _italic_, etc.
+            </small>
+          )}
+        </>
+      );
+    } else {
+      return (
+        <span className={field === "name" ? "font-bold italic" : ""}>
+          {" "}
+          {value}
+        </span>
+      );
+    }
+  };
 
-    return (
-        <div className={`${editMode && 'pt-4'}`}>
-            {title && <h3 className={`text-lg uppercase ${editMode && 'text-light'}`}>{title}</h3>}
-            {!editMode && <TaperedRule />}
-            {desc && <p>{desc}</p>}
-            {actions.map((action, index) => (
-                <div key={index} className="pb-4">
-                    <div className="text-black">
-                        {renderActionName(index, 'name', action.name)}
-                        {renderActionDesc(index, 'desc', action.desc)}
-                        {isFocused &&
-                            <small className="text-gray-500">* Supports Markdown. Use **bold**, _italic_, etc.</small>
-                        }
-                    </div>
-                </div>
-            ))}
+  return (
+    <div className={`${editMode && "pt-4"}`}>
+      {title && (
+        <h3 className={`text-lg uppercase ${editMode && "text-light"}`}>
+          {title}
+        </h3>
+      )}
+      {!editMode && <TaperedRule />}
+      {desc && handleDesc && (
+        <PropertyLineString
+          label={`${editMode ? "Legendary description" : ""}`}
+          value={desc}
+          onChange={(value) => handleDesc(value)}
+          editMode={editMode}
+        />
+      )}
+      {actions.map((action, index) => (
+        <div key={index} className="pb-4">
+          <div className="text-black">
+            {renderActionName(index, "name", action.name)}
+            {renderActionDesc(index, "desc", action.desc)}
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
-export default Action
+export default Action;
--- src/components/StatBlock/index.tsx
+++ src/components/StatBlock/index.tsx
@@ -1,28 +1,31 @@
 "use client";
-import React, { useEffect, useState } from 'react';
-import AbilitiesBlock from './CustomBlocks/AbilitiesBlock';
-import TaperedRule from './CustomBlocks/TaperedRule';
-import SpecialAbility from './CustomBlocks/SpecialAbility';
-import Action from './CustomBlocks/Action';
-import Description from './CustomBlocks/Description';
-import PropertyLineString from './CustomBlocks/PropertyLineString';
-import PropertyLineNb from './CustomBlocks/PropertyLineNb';
+import React, { useEffect, useState } from "react";
+import AbilitiesBlock from "./CustomBlocks/AbilitiesBlock";
+import TaperedRule from "./CustomBlocks/TaperedRule";
+import SpecialAbility from "./CustomBlocks/SpecialAbility";
+import Action from "./CustomBlocks/Action";
+import Description from "./CustomBlocks/Description";
+import PropertyLineString from "./CustomBlocks/PropertyLineString";
+import PropertyLineNb from "./CustomBlocks/PropertyLineNb";
 
 interface StatBlockComponentProps {
   monster: StatBlockProps;
   editMode: boolean;
 }
 
-const StatBlockComponent: React.FC<StatBlockComponentProps> = ({ monster, editMode }) => {
+const StatBlockComponent: React.FC<StatBlockComponentProps> = ({
+  monster,
+  editMode,
+}) => {
   const [monsterBlock, setMonsterBlock] = useState<StatBlockProps>(monster);
 
   const attributeAbbreviations = {
-    strength_save: 'Str',
-    dexterity_save: 'Dex',
-    constitution_save: 'Con',
-    intelligence_save: 'Int',
-    wisdom_save: 'Wis',
-    charisma_save: 'Cha',
+    strength_save: "Str",
+    dexterity_save: "Dex",
+    constitution_save: "Con",
+    intelligence_save: "Int",
+    wisdom_save: "Wis",
+    charisma_save: "Cha",
   };
 
   const formatAttributes = (attributes: number, abbreviations: string) => {
@@ -30,35 +33,71 @@ const StatBlockComponent: React.FC<StatBlockComponentProps> = ({ monster, editMo
       .filter(([key, value]) => value !== null)
       .map(([key, value]) => {
         const attributeAbbr = abbreviations[key] || key;
-        return `${attributeAbbr} ${value >= 0 ? '+' : ''} ${value}`;
+        return `${attributeAbbr} ${value >= 0 ? "+" : ""} ${value}`;
       })
-      .join(', ');
+      .join(", ");
   };
 
   const calculateModifier = (score: number) => Math.floor((score - 10) / 2);
 
   const savingThrows = {
-    strength_save: monsterBlock.strength_save !== null && monsterBlock.strength !== undefined ? calculateModifier(monsterBlock.strength) + (monsterBlock.strength_save ?? 0) : null,
-    dexterity_save: monsterBlock.dexterity_save !== null && monsterBlock.dexterity !== undefined ? calculateModifier(monsterBlock.dexterity) + (monsterBlock.dexterity_save ?? 0) : null,
-    constitution_save: monsterBlock.constitution_save !== null && monsterBlock.constitution !== undefined ? calculateModifier(monsterBlock.constitution) + (monsterBlock.constitution_save ?? 0) : null,
-    intelligence_save: monsterBlock.intelligence_save !== null && monsterBlock.intelligence !== undefined ? calculateModifier(monsterBlock.intelligence) + (monsterBlock.intelligence_save ?? 0) : null,
-    wisdom_save: monsterBlock.wisdom_save !== null && monsterBlock.wisdom !== undefined ? calculateModifier(monsterBlock.wisdom) + (monsterBlock.wisdom_save ?? 0) : null,
-    charisma_save: monsterBlock.charisma_save !== null && monsterBlock.charisma !== undefined ? calculateModifier(monsterBlock.charisma) + (monsterBlock.charisma_save ?? 0) : null,
+    strength_save:
+      monsterBlock.strength_save !== null && monsterBlock.strength !== undefined
+        ? calculateModifier(monsterBlock.strength) +
+          (monsterBlock.strength_save ?? 0)
+        : null,
+    dexterity_save:
+      monsterBlock.dexterity_save !== null &&
+      monsterBlock.dexterity !== undefined
+        ? calculateModifier(monsterBlock.dexterity) +
+          (monsterBlock.dexterity_save ?? 0)
+        : null,
+    constitution_save:
+      monsterBlock.constitution_save !== null &&
+      monsterBlock.constitution !== undefined
+        ? calculateModifier(monsterBlock.constitution) +
+          (monsterBlock.constitution_save ?? 0)
+        : null,
+    intelligence_save:
+      monsterBlock.intelligence_save !== null &&
+      monsterBlock.intelligence !== undefined
+        ? calculateModifier(monsterBlock.intelligence) +
+          (monsterBlock.intelligence_save ?? 0)
+        : null,
+    wisdom_save:
+      monsterBlock.wisdom_save !== null && monsterBlock.wisdom !== undefined
+        ? calculateModifier(monsterBlock.wisdom) +
+          (monsterBlock.wisdom_save ?? 0)
+        : null,
+    charisma_save:
+      monsterBlock.charisma_save !== null && monsterBlock.charisma !== undefined
+        ? calculateModifier(monsterBlock.charisma) +
+          (monsterBlock.charisma_save ?? 0)
+        : null,
   };
-  const formattedSpeedText = Object.entries(monsterBlock.speed ?? {}).map(([key, value]) => `${key} ${value}`).join(', ');
-  const formattedSavingThrows = formatAttributes(savingThrows, attributeAbbreviations);
+  const formattedSpeedText = Object.entries(monsterBlock.speed ?? {})
+    .map(([key, value]) => `${key} ${value}`)
+    .join(", ");
+  const formattedSavingThrows = formatAttributes(
+    savingThrows,
+    attributeAbbreviations
+  );
   const formattedSkills = formatAttributes(monsterBlock.skills, {});
-  const [savingThrowsText, setsavingThrowsText] = useState<string>(formattedSavingThrows);
+  const [savingThrowsText, setsavingThrowsText] = useState<string>(
+    formattedSavingThrows
+  );
   const [skillsText, setSkillsText] = useState<string>(formattedSkills);
   const [speedText, setSpeedText] = useState<string>(formattedSpeedText);
-  const [typeText, setTypeText] = useState<string>(`${monsterBlock.size} ${monsterBlock.type}, ${monsterBlock.alignment}`);
+  const [typeText, setTypeText] = useState<string>(
+    `${monsterBlock.size} ${monsterBlock.type}, ${monsterBlock.alignment}`
+  );
 
   const handleTextChange = (field: string, value: string) => {
-    if (field === 'saving_throws') setsavingThrowsText(value);
-    else if (field === 'skills') setSkillsText(value);
-    else if (field === 'type') setTypeText(value);
-    else if (field === 'speed') setSpeedText(value);
-    else return
+    if (field === "saving_throws") setsavingThrowsText(value);
+    else if (field === "skills") setSkillsText(value);
+    else if (field === "type") setTypeText(value);
+    else if (field === "speed") setSpeedText(value);
+    else return;
   };
 
   const handleChange = (field: string, value: any) => {
@@ -67,39 +106,83 @@ const StatBlockComponent: React.FC<StatBlockComponentProps> = ({ monster, editMo
       [field]: value,
     }));
   };
+  const handleActionChange = (actionType:string, index: number, field: string, value: string) => {
+    setMonsterBlock((prevMonster) => ({
+        ...prevMonster,
+        [actionType]: prevMonster[actionType].map((action, i) => 
+            i === index ? { ...action, [field]: value } : action
+        ),
+    }));
+};
 
-  useEffect(() => {
-
-  }, [editMode])
+  useEffect(() => {}, [editMode]);
 
   useEffect(() => {
     setMonsterBlock(monster);
     console.log(monsterBlock);
   }, [monster]);
 
-
   useEffect(() => {
     setsavingThrowsText(formatAttributes(savingThrows, attributeAbbreviations));
     setSkillsText(formatAttributes(monsterBlock.skills, {}));
-    setTypeText(`${monsterBlock.size} ${monsterBlock.type}, ${monsterBlock.alignment}`);
+    setTypeText(
+      `${monsterBlock.size} ${monsterBlock.type}, ${monsterBlock.alignment}`
+    );
     setSpeedText(formattedSpeedText);
   }, [monsterBlock]);
 
   return (
-    <div className={`w-full md:w-[750px] lg:w-[950px] overflow-clip self-center ${editMode && ''}`}>
-      <div className={`h-[5px] border-b-[1px] border-black bg-statBlockOrange ${editMode && 'hidden'}`} />
-      <div className={` ${editMode ? 'md:flex flex-col gap 8 text-sm md:text-md text-dark' : 'md:columns-2 gap-5 p-6 bg-statBlockBG shadow-lg text-statBlockRed text-sm'}`}>
-        <PropertyLineString label={editMode ? 'Monster Name' : ''} value={monsterBlock.name} onChange={(value) => handleChange('name', value)} classname={'text-2xl font-bold uppercase'} editMode={editMode} />
-        <PropertyLineString label={editMode ? 'Type, size and Alignment' : ''} value={typeText} onChange={(value) => handleTextChange('type', value)} classname={'italic'} editMode={editMode} />
-        {!editMode &&
-          <TaperedRule />
-        }
-        <PropertyLineNb label='Armor Class' value={monsterBlock.armor_class} onChange={(value) => handleChange('armor_class', value)} editMode={editMode} />
-        <PropertyLineNb label='Hit Points' value={monsterBlock.hit_points} onChange={(value) => handleChange('hit_points', value)} editMode={editMode} />
-        <PropertyLineString label='Speed' value={speedText} onChange={(value) => handleTextChange('speed', value)} editMode={editMode} />
-        {!editMode &&
-          <TaperedRule />
-        }
+    <div
+      className={`w-full md:w-[750px] lg:w-[950px] overflow-clip self-center ${
+        editMode && ""
+      }`}
+    >
+      <div
+        className={`h-[5px] border-b-[1px] border-black bg-statBlockOrange ${
+          editMode && "hidden"
+        }`}
+      />
+      <div
+        className={` ${
+          editMode
+            ? "md:flex flex-col gap 8 text-sm md:text-md text-dark"
+            : "md:columns-2 gap-5 p-6 bg-statBlockBG shadow-lg text-statBlockRed text-sm"
+        }`}
+      >
+        <PropertyLineString
+          label={editMode ? "Monster Name" : ""}
+          value={monsterBlock.name}
+          onChange={(value) => handleChange("name", value)}
+          classname={"text-2xl font-bold uppercase"}
+          editMode={editMode}
+        />
+        <PropertyLineString
+          label={editMode ? "Type, size and Alignment" : ""}
+          value={typeText}
+          onChange={(value) => handleTextChange("type", value)}
+          classname={"italic"}
+          editMode={editMode}
+        />
+        {!editMode && <TaperedRule />}
+        <PropertyLineNb
+          label="Armor Class"
+          value={monsterBlock.armor_class}
+          onChange={(value) => handleChange("armor_class", value)}
+          editMode={editMode}
+        />
+        <PropertyLineNb
+          label="Hit Points"
+          value={monsterBlock.hit_points}
+          onChange={(value) => handleChange("hit_points", value)}
+          editMode={editMode}
+        />
+        <PropertyLineString
+          label="Speed"
+          value={speedText}
+          onChange={(value) => handleTextChange("speed", value)}
+          editMode={editMode}
+        />
+        {!editMode && <TaperedRule />}
         <AbilitiesBlock
           strength={monsterBlock.strength}
           dexterity={monsterBlock.dexterity}
@@ -110,26 +193,107 @@ const StatBlockComponent: React.FC<StatBlockComponentProps> = ({ monster, editMo
           handleChange={handleChange}
           editMode={editMode}
         />
-        {!editMode &&
-          <TaperedRule />
-        }
-        <PropertyLineString label='Saving Throws' value={savingThrowsText} onChange={(value) => handleTextChange('saving_throws', value)} editMode={editMode} />
-        <PropertyLineString label='Skills' value={skillsText} onChange={(value) => handleTextChange('skills', value)} editMode={editMode} />
-        <PropertyLineString label='Damage Vulnerabilities' value={monsterBlock.damage_vulnerabilities} onChange={(value) => handleChange('damage_vulnerabilities', value)} editMode={editMode} />
-        <PropertyLineString label='Damage Resistances' value={monsterBlock.damage_resistances} onChange={(value) => handleChange('damage_resistances', value)} editMode={editMode} />
-        <PropertyLineString label='Damage Immunities' value={monsterBlock.damage_immunities} onChange={(value) => handleChange('damage_immunities', value)} editMode={editMode} />
-        <PropertyLineString label='Condition Immunities' value={monsterBlock.condition_immunities} onChange={(value) => handleChange('condition_immunities', value)} editMode={editMode} />
-        <PropertyLineString label='Senses' value={monsterBlock.senses} onChange={(value) => handleChange('senses', value)} editMode={editMode} />
-        <PropertyLineString label='Languages' value={monsterBlock.languages} onChange={(value) => handleChange('languages', value)} editMode={editMode} />
-        <PropertyLineNb label='Challenge' value={monsterBlock.cr} onChange={(value) => handleChange('challenge_rating', value)} editMode={editMode} />
-        <Action actions={monsterBlock.special_abilities} title={editMode ? 'Special Abilities' : ''} editMode={editMode} />
-        <Action actions={monsterBlock.actions} title='Actions' editMode={editMode} />
-        <Action actions={monsterBlock.bonus_actions} title='Bonus Actions' editMode={editMode} />
-        <Action actions={monsterBlock.reactions} title='Reactions' editMode={editMode} />
-        <Action actions={monsterBlock.legendary_actions} desc={monsterBlock.legendary_desc} title='Legendary Actions' editMode={editMode} />
-        <PropertyLineString label={editMode ? 'Description' : ''} value={monsterBlock.desc} editMode={editMode} onChange={(value) => handleChange('desc', value)} type='textarea' classname='italic' />
+        {!editMode && <TaperedRule />}
+        <PropertyLineString
+          label="Saving Throws"
+          value={savingThrowsText}
+          onChange={(value) => handleTextChange("saving_throws", value)}
+          editMode={editMode}
+        />
+        <PropertyLineString
+          label="Skills"
+          value={skillsText}
+          onChange={(value) => handleTextChange("skills", value)}
+          editMode={editMode}
+        />
+        <PropertyLineString
+          label="Damage Vulnerabilities"
+          value={monsterBlock.damage_vulnerabilities}
+          onChange={(value) => handleChange("damage_vulnerabilities", value)}
+          editMode={editMode}
+        />
+        <PropertyLineString
+          label="Damage Resistances"
+          value={monsterBlock.damage_resistances}
+          onChange={(value) => handleChange("damage_resistances", value)}
+          editMode={editMode}
+        />
+        <PropertyLineString
+          label="Damage Immunities"
+          value={monsterBlock.damage_immunities}
+          onChange={(value) => handleChange("damage_immunities", value)}
+          editMode={editMode}
+        />
+        <PropertyLineString
+          label="Condition Immunities"
+          value={monsterBlock.condition_immunities}
+          onChange={(value) => handleChange("condition_immunities", value)}
+          editMode={editMode}
+        />
+        <PropertyLineString
+          label="Senses"
+          value={monsterBlock.senses}
+          onChange={(value) => handleChange("senses", value)}
+          editMode={editMode}
+        />
+        <PropertyLineString
+          label="Languages"
+          value={monsterBlock.languages}
+          onChange={(value) => handleChange("languages", value)}
+          editMode={editMode}
+        />
+        <PropertyLineNb
+          label="Challenge"
+          value={monsterBlock.cr}
+          onChange={(value) => handleChange("challenge_rating", value)}
+          editMode={editMode}
+        />
+        <Action
+          actions={monsterBlock.special_abilities}
+          title={editMode ? "Special Abilities" : ""}
+          editMode={editMode}
+          onChange={(index, field, value) => handleActionChange("special_abilities", index, field, value)}
+        />
+        <Action
+          actions={monsterBlock.actions}
+          title="Actions"
+          editMode={editMode}
+          onChange={(index, field, value) => handleActionChange("special_abilities", index, field, value)}
+        />
+        <Action
+          actions={monsterBlock.bonus_actions}
+          title="Bonus Actions"
+          editMode={editMode}
+          onChange={(index, field, value) => handleActionChange("bonus_actions", index, field, value)}
+        />
+        <Action
+          actions={monsterBlock.reactions}
+          title="Reactions"
+          editMode={editMode}
+          onChange={(index, field, value) => handleActionChange("monsterBlock", index, field, value)}
+        />
+        <Action
+          actions={monsterBlock.legendary_actions}
+          desc={monsterBlock.legendary_desc}
+          title="Legendary Actions"
+          editMode={editMode}
+          onChange={(index, field, value) => handleActionChange("legendary_actions", index, field, value)}
+          handleDesc={(value) => handleChange("legendary_desc", value)}
+        />
+        <PropertyLineString
+          label={editMode ? "Description" : ""}
+          value={monsterBlock.desc}
+          editMode={editMode}
+          onChange={(value) => handleChange("desc", value)}
+          type="textarea"
+          classname="italic"
+        />
       </div>
-      <div className={`h-[5px] border-b-[1px] border-black bg-statBlockOrange ${editMode === true && 'hidden'}`} />
+      <div
+        className={`h-[5px] border-b-[1px] border-black bg-statBlockOrange ${
+          editMode === true && "hidden"
+        }`}
+      />
     </div>
   );
 };
